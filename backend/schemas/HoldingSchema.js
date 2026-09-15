1fb8ecaf1aead65af86cf3114f99014e10227d72
abhay pratap <abhaygan122@gmail.com>
2026-09-15 23:46:35 +0530

    hello

--- backend/schemas/HoldingSchema.js
+++ backend/schemas/HoldingSchema.js
@@ -1,11 +1,11 @@
-const {Schema} = require('mongoose');
-
-const HoldingSchema=new Schema({
-    name: String,
-    qty: Number,
-    avg: Number,
-    price: Number,
-    net: String,
-    day: String,
+const { Schema } = require("mongoose");
+// hello
+const HoldingSchema = new Schema({
+  name: String,
+  qty: Number,
+  avg: Number,
+  price: Number,
+  net: String,
+  day: String,
 });
-module.exports={HoldingSchema};
+module.exports = { HoldingSchema };
