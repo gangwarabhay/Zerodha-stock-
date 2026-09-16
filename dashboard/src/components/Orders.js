46248674441b9a5eab4be9f5b9005db9e3409c6d
abhay pratap <abhaygan122@gmail.com>
2026-09-16 16:47:39 +0530

    fixed

--- dashboard/src/components/Orders.js
+++ dashboard/src/components/Orders.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import axios from "axios";
-import { Link } from "react-router-dom";
 
 const Orders = () => {
   const [allOrders, setAllOrderss] = useState([]);
